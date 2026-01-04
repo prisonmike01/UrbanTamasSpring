d8c04bd5610b3871d3375147ef339969a194b261
prisonmike <utamas2003@gmail.com>
2026-01-04 21:05:26 +0100

    inital commit with spring boot

---
+++ src/test/java/com/UrbanTamas/demo/SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UrbanTamas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
